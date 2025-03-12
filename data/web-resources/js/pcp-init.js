dc91697a002a38376bc4e915b9fa03e9a378bb3a
Prakash Venkatesh <prakash.venkatesh@perficient.com>
2025-03-11 17:28:43 -0700

    adding by w full and h full

--- data/web-resources/js/pcp-init.js
+++ data/web-resources/js/pcp-init.js
@@ -158,6 +158,7 @@ import { addProductToCart } from './productUtilities.js';
           <img
             src="${item.imgSrc}"
             alt="${item.name}"
+            class="w-full h-full object-contain"
           />
         </div>
         <div class="p-4">
