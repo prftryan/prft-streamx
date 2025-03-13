f317b1b61e3986f740b1a25ab9d1ffbaef52bfd9
Prakash Venkatesh <prakash.venkatesh@perficient.com>
2025-03-13 07:46:51 -0700

    fixing the name for search dropdown

--- terraform/scripts/cloud-publish-all.sh
+++ terraform/scripts/cloud-publish-all.sh
@@ -5,6 +5,6 @@ export QUARKUS_PROFILE=cloud,cms && streamx batch publish data
 #export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/products.stream
 #export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/categories.stream
 # export QUARKUS_PROFILE=cloud,cms && streamx stream renderers ../lumaxscript/unpublish-renderers.stream
-
+# pim (data channel), cms (renderers),
 
 popd || exit
