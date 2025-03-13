d36e0a7df26a62fae72bec9f281a80da90274f02
Prakash Venkatesh <prakash.venkatesh@perficient.com>
2025-03-13 06:48:18 -0700

    correcting the syntax in yaml file

--- terraform/scripts/cloud-publish-all.sh
+++ terraform/scripts/cloud-publish-all.sh
@@ -4,5 +4,7 @@ pushd "${SCRIPT_DIR}/../../" || exit
 export QUARKUS_PROFILE=cloud,cms && streamx batch publish data
 #export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/products.stream
 #export QUARKUS_PROFILE=cloud,pim && streamx stream data data/catalog/categories.stream
+# export QUARKUS_PROFILE=cloud,cms && streamx stream renderers ../lumaxscript/unpublish-renderers.stream
+
 
 popd || exit
